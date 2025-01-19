f58019309d256a275ced6c50c75e26e755196943
CarlosBedoyaRestrepo <carlosbedoyarestrepo5@gmail.com>
2025-01-19 17:06:50 -0500

    En esta version se muestra un ejercicio de bases de datos con el esquema BaseModel, esto hace que la informacion este localizada en memoria

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from datetime import datetime
 import zoneinfo
+from models import Customer,CustomerCreate, Transaction, Invoice
+
 
 
 app=FastAPI()
@@ -26,3 +28,29 @@ async def time(iso_code: str):
     timezone_str=country_timezone.get(iso)
     tz=zoneinfo.ZoneInfo(timezone_str)
     return {"time":datetime.now(tz)}
+
+
+#esto se supone que es nuestra base de datos, que es la lista de customers
+db_customers:list[Customer]=[]
+
+
+@app.post("/customers",response_model=Customer)
+async def create_customer(customer_data:CustomerCreate):
+    customer=Customer.model_validate(customer_data.model_dump())#esto almacena en un diccionario
+    customer.id=len(db_customers)
+    db_customers.append(customer)
+    return customer
+
+
+@app.get("/customers",response_model=list[Customer])
+async def list_customer():
+    return db_customers
+
+
+@app.post("/transactions")
+async def create_transactions(transaction_data:Transaction):
+    return transaction_data
+
+@app.post("/invoices")
+async def create_invoice(invoice_data:Invoice):
+    return invoice_data
