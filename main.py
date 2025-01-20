32fc9a7d3b144805115adef2aacee9cd3da90c31
CarlosBedoyaRestrepo <carlosbedoyarestrepo5@gmail.com>
2025-01-19 22:38:01 -0500

    En esta clase se enseña la conexion con bases de datos, especificamente sqlite3, se muestra el manejo del objeto session, el cual contiene los cuatro metodos mas importantes:get, post, delete y patch. Ademas, se muestra como se crea y manipula algunas funcionalidades de la libreria sqlmodel

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
 from datetime import datetime
 import zoneinfo
-from models import Customer,CustomerCreate, Transaction, Invoice
+from models import Customer,CustomerCreate, Transaction, Invoice,CustomerUpdate
+from db import SessionDep,create_all_tables
+from sqlmodel import select
 
 
-
-app=FastAPI()
+app=FastAPI(lifespan=create_all_tables)
 
 @app.get("/")
 async def root():
     return {"message":"hola mundo"}
-
-
-
 country_timezone={
     "CO":"America/Bogota",
     "MX":"America/Mexico_City",
@@ -20,8 +18,6 @@ country_timezone={
     "BR":"America/Sao_Paulo",
     "PE":"America/Lima",
 }
-
-
 @app.get("/time/{iso_code}")
 async def time(iso_code: str):
     iso=iso_code.upper()
@@ -30,23 +26,59 @@ async def time(iso_code: str):
     return {"time":datetime.now(tz)}
 
 
-#esto se supone que es nuestra base de datos, que es la lista de customers
-db_customers:list[Customer]=[]
 
 
-@app.post("/customers",response_model=Customer)
-async def create_customer(customer_data:CustomerCreate):
+
+
+@app.post("/customers",response_model=Customer)           #session es una clase que pérmite hacer querys
+async def create_customer(customer_data:CustomerCreate,session:SessionDep): 
     customer=Customer.model_validate(customer_data.model_dump())#esto almacena en un diccionario
-    customer.id=len(db_customers)
-    db_customers.append(customer)
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
     return customer
 
+@app.get("/customers/{customer_id}",response_model=Customer)
+async def read_customer(customer_id:int,session:SessionDep):
+    customer_db=session.get(Customer,customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Customer doesn't exist")
+    return customer_db
+
+@app.patch("/customers/{customer_id}",response_model=Customer,status_code=status.HTTP_201_CREATED)
+async def read_customer(customer_id:int,customer_data: CustomerUpdate,session:SessionDep):
+    customer_db=session.get(Customer,customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Customer doesn't exist")
+    
+    customer_data_dict=customer_data.model_dump(exclude_unset=True)
+    customer_db.sqlmodel_update(customer_data_dict)
+    session.add(customer_db)
+    session.commit()
+    session.refresh(customer_db)
+    return customer_db
+
+@app.delete("/customers/{customer_id}")
+async def read_customer(customer_id:int,session:SessionDep):
+    customer_db=session.get(Customer,customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Customer doesn't exist")
+    session.delete(customer_db)
+    session.commit()
+    return {"detail":"ok"}
 
 @app.get("/customers",response_model=list[Customer])
-async def list_customer():
-    return db_customers
+async def list_customer(session:SessionDep):
+    return session.exec(select(Customer)).all()
 
 
+
+
+
+
+
+
+#Por ahora estos no estan muy elaborados
 @app.post("/transactions")
 async def create_transactions(transaction_data:Transaction):
     return transaction_data
--- models.py
+++ models.py
@@ -1,16 +1,29 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel,Field
 
-class CustomerBase(BaseModel):
-    name:str
-    description:str | None 
-    email:str
-    age:int
 
-class CustomerCreate(CustomerBase):
+
+class CustomerBase(SQLModel):
+    name:str=Field(default=None)
+    description:str | None =Field(default=None)
+    email:str=Field(default=None)
+    age:int=Field(default=None)
+
+class CustomerCreate(CustomerBase): #esto es porque cuando se crea aveces necesita parametros de entrada como la contraseña
     pass
 
-class Customer(CustomerBase):
-    id:int | None =None
+
+class CustomerUpdate(CustomerBase): #esto es porque cuando se crea aveces necesita parametros de entrada como la contraseña
+    pass
+
+class Customer(CustomerBase,table=True):
+    id:int | None =Field(default=None,primary_key=True)
+
+
+
+
+
+#Para esta clase los modelos de abajo no se usan
 
 class Transaction(BaseModel):
     id: int
